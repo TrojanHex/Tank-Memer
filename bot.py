e945f686b547f9071d446995c5f8586e9e17726b
Saksham Arya <aryasaksham@gmail.com>
2021-04-27 17:27:36 +0530

    Initial Structure

---
+++ bot.py
@@ -0,0 +1,38 @@
+import tweepy
+import os
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+consumer_key = os.getenv("consumer_key")
+consumer_secret = os.getenv("consumer_secret")
+acess_token = os.getenv("acess_token")
+acess_secret = os.getenv("acess_secret")
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(acess_token, acess_secret)
+api = tweepy.API(auth)
+
+mention=api.mentions_timeline(1)[0]
+screen_name = mention.author.screen_name
+id = mention.id_str
+text=mention.text
+
+filename = "temp.jpg"
+url = "https://preview.redd.it/d6i4k905snv61.jpg?width=640&height=424&crop=smart&auto=webp&s=bac4a75feb6d00e50139c0b54b1a96aaff877ccf"
+request = requests.get(url, stream=True)
+
+if request.status_code == 200:
+    with open(filename, 'wb') as image:
+        for chunk in request:
+            image.write(chunk)
+    image=api.media_upload(filename)
+    os.remove(filename)
+
+media_id=image.media_id_string
+api.update_status(status='@'+screen_name,in_reply_to_status_id=id,media_ids=[media_id])
+
+
+
