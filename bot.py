d0f0bf54faee2b18a4dc566bda6a14387bab639e
Saksham Arya <aryasaksham@gmail.com>
2021-05-03 16:34:51 +0530

    created a streaming based one

--- bot.py
+++ bot.py
@@ -1,52 +1,38 @@
 import tweepy
 import os
 import requests
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 import praw
 import random
-import pickle
-from sys import argv
-# load_dotenv()
+import sys
+from tweepy.streaming import StreamListener
 
-consumer_key = argv[1]
-consumer_secret = argv[2]
-acess_token = argv[3]
-acess_secret = argv[4]
-client_id=argv[5]
-client_secret=argv[6]
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(acess_token, acess_secret)
-api = tweepy.API(auth)
-mentions = api.mentions_timeline(count=20)
-check=[]    
-# with open('parrot.pkl', 'wb') as f:
-#     pickle.dump(mentions.id, f)
-if os.path.getsize('all_mentions') > 0:  
-    with open('all_mentions', 'rb') as f:
-        check = pickle.load(f)
-print(check)
-for mention in mentions:
-    if mention.id not in check:
+class Listener(StreamListener):
+    def __init__(self, output_file=sys.stdout):
+        super(Listener, self).__init__()
+        self.output_file = output_file
+
+    def on_status(self, status):
         print("processing image")
-        screen_name = mention.author.screen_name
-        id = mention.id_str
-        text = mention.text.split(" ") 
+        screen_name = status.author.screen_name
+        id = status.id_str
+        text = status.text.split(" ")
 
-        if len(text)<2 :
-            text="dankmemes"
+        if len(text) < 2:
+            text = "dankmemes"
         else:
-            text=text[1]
-        
+            text = text[1]
+
         filename = "temp.jpg"
-        reddit = praw.Reddit(client_id = client_id, 
-                     client_secret = client_secret, 
-                     user_agent = 'meme-scraper')
+        reddit = praw.Reddit(client_id=client_id,
+                             client_secret=client_secret,
+                             user_agent='meme-scraper')
         try:
-            subreddit=reddit.subreddit(text)
+            subreddit = reddit.subreddit(text)
             posts = subreddit.hot(limit=10)
-            urls=[post.url for post in posts if "https://v.redd.it" not in post.url and ".gif" not in post.url]
-            url=random.sample(urls,k=1)[0]
+            urls = [post.url for post in posts if "https://v.redd.it" not in post.url and ".gif" not in post.url]
+            url = random.sample(urls, k=1)[0]
 
             request = requests.get(url, stream=True)
             if request.status_code == 200:
@@ -55,17 +41,29 @@ for mention in mentions:
                         image.write(chunk)
                 image = api.media_upload(filename)
                 os.remove(filename)
-                check.append(mention.id)
-                media_id=image.media_id_string
-                api.update_status(status='@'+screen_name,in_reply_to_status_id=id,media_ids=[media_id])
+                media_id = image.media_id_string
+                api.update_status(status='@'+screen_name,
+                                  in_reply_to_status_id=id, media_ids=[media_id])
                 print("sent image")
-
         except Exception as e:
-            print("invalid subreddit name.skipping...")
+            print(e)
 
+load_dotenv()
 
-        
-with open('all_mentions', 'wb') as f:
-    pickle.dump(check, f)
+consumer_key = os.getenv("consumer_key")
+consumer_secret = os.getenv("consumer_secret")
+acess_token = os.getenv("acess_token")
+acess_secret = os.getenv("acess_secret")
+client_id = os.getenv("client_id")
+client_secret = os.getenv("client_secret")
 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(acess_token, acess_secret)
+api = tweepy.API(auth)
+mentions = api.mentions_timeline(count=20)
+check = []
 
+listener = Listener()
+
+stream = tweepy.Stream(auth=auth, listener=listener)
+tweets = stream.filter(track=["@tankmemer"])
