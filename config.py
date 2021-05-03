288a66c1d79166b553b85728581271d2e5d6d017
tanmyavishvakarma <tanmya2000@gmail.com>
2021-05-03 13:48:10 +0530

    Added config

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = "xKBD2JSQ75EpbmCMXWJZ8QMzl"
+consumer_secret = "JnP0AuHszwsOOWQP1ILF5Axut1vG1KaVpCmQ3ifDnAcvmFwUBx"
+acess_token = "1386990443334828039-GUa1azeNnidtg4DWpjLH8fXG5xhd2j"
+acess_secret = "HM9OnW5B9WhSFJCDqH8i20DdPVf8IoewZyJx9pmqNlAUU"
